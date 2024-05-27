6fd53f0e738850054c98fa7cf93cdfffdb356f2d
Jesper Molin <jesper.molin@teliacompany.com>
2024-05-27 20:37:37 +0200

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,3 +1,5 @@
+const tabs = document.querySelectorAll(".tablinks");
+if(tabs) tabs[0].style.fontWeight = "600";
 function openTab(index, id) { 
     const tabContents = document.querySelectorAll(".tabcontent");
     tabContents.forEach(tabContent => {
@@ -5,4 +7,11 @@ function openTab(index, id) {
     })
     const activeContent = document.querySelector(`#${id}`);
     activeContent.style.display = "block";
+
+    const tabs = document.querySelectorAll(".tablinks");
+    tabs.forEach(tab => {
+        tab.style.fontWeight = "normal";
+    })
+    const activeTab = tabs[index-1];
+    activeTab.style.fontWeight = "600";
 }
